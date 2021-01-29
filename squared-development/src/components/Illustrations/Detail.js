5119bcce662dff4b530e34887e80d6a7ad28a3f2
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2021-01-29 22:07:34 +0100

    added test for Detail component

--- squared-development/src/components/Illustrations/Detail.js
+++ squared-development/src/components/Illustrations/Detail.js
@@ -12,13 +12,14 @@ const rotate = keyframes`
 `
 
 const DetailContainer = styled.div`
-  width:${props => props.size * 2 + 'vw'};
+  width:${props => (props.size ? props.size : 1) * 2 + 'vw'};
   position:absolute;
   ${props => css`
-    ${props.verticalPosition}: 0%;
-    ${props.horizontalPosition}: 0%;
+    ${props.verticalPosition === 'top' ? props.verticalPosition : (props.verticalPosition === 'bottom' ? props.verticalPosition : 'top')}: 0%;
+    ${props.horizontalPosition === 'left' ? props.horizontalPosition : (props.horizontalPosition === 'right' ? props.horizontalPosition : 'left')}: 0%;
   `};
-  animation:${rotate} ${props => props.size / 0.5}s linear infinite;
+  animation:${rotate} linear infinite;
+  animation-duration: ${props => props.size / 0.5}s;
   animation-direction: ${props => props.backwards ? 'reverse' : 'normal'};
 `
 
@@ -32,13 +33,20 @@ const StyledRect = styled.rect`
 
 const Detail = ({size, verticalPosition, horizontalPosition, backwards, ...rest}) => {
   return(
-    <DetailContainer size={size} backwards={backwards} {...rest} verticalPosition={verticalPosition} horizontalPosition={horizontalPosition}>
+    <DetailContainer 
+      size={size} 
+      backwards={backwards} 
+      {...rest} 
+      verticalPosition={verticalPosition} 
+      horizontalPosition={horizontalPosition}
+      data-testid="detail"
+    >
       <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 424.537 424.537">
         <g id="Group_31" data-name="Group 31" transform="translate(-1445.897 -4265.975) rotate(45)" opacity="0.19">
-          <StyledRect id="Rectangle_69" data-name="Rectangle 69" width="121.953" height="121.953" transform="translate(4189 1965.953) rotate(-90)"/>
-          <StyledRect id="Rectangle_70" data-name="Rectangle 70" width="121.953" height="121.953" transform="translate(4367.239 1965.953) rotate(-90)"/>
-          <StyledRect id="Rectangle_71" data-name="Rectangle 71" width="121.953" height="121.953" transform="translate(4189 2144.193) rotate(-90)"/>
-          <StyledRect id="Rectangle_72" data-name="Rectangle 72" width="121.953" height="121.953" transform="translate(4367.239 2144.193) rotate(-90)"/>
+          <StyledRect data-testid='styled-rect' id="Rectangle_69" data-name="Rectangle 69" width="121.953" height="121.953" transform="translate(4189 1965.953) rotate(-90)"/>
+          <StyledRect data-testid='styled-rect' id="Rectangle_70" data-name="Rectangle 70" width="121.953" height="121.953" transform="translate(4367.239 1965.953) rotate(-90)"/>
+          <StyledRect data-testid='styled-rect' id="Rectangle_71" data-name="Rectangle 71" width="121.953" height="121.953" transform="translate(4189 2144.193) rotate(-90)"/>
+          <StyledRect data-testid='styled-rect' id="Rectangle_72" data-name="Rectangle 72" width="121.953" height="121.953" transform="translate(4367.239 2144.193) rotate(-90)"/>
         </g>
       </svg>
     </DetailContainer>
---
+++ squared-development/src/components/Illustrations/Detail.test.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import Detail from './Detail'
+import {theme} from '../../../gatsby-browser' 
+
+import {ThemeProvider} from 'styled-components'
+
+describe('Detail', () => {
+
+  const DetailWithTheme = ({...props}) => <ThemeProvider theme={theme}>
+    <Detail {...props}/>
+  </ThemeProvider>
+
+  it('renders', () => {
+    const {getByTestId} = render(<DetailWithTheme/>)
+    expect(getByTestId('detail')).not.toBe(null)
+  })
+  
+  it('accepts backwards prop', () => {
+    const {getByTestId} = render(<DetailWithTheme backwards={true}/>)
+    expect(getByTestId('detail')).toHaveStyle(`
+      animation-direction: reverse;
+    `)
+  })
+  
+  it('have normal animation direction without backwards prop', () => {
+    const {getByTestId} = render(<DetailWithTheme />)
+    expect(getByTestId('detail')).toHaveStyle(`
+      animation-direction: normal;
+    `)
+  })
+  
+  it('accepts size prop', () => {
+    const {getByTestId} = render(<DetailWithTheme size={1}/>)
+    expect(getByTestId('detail')).toHaveStyle(`
+      width: ${1 * 2}vw;
+    `)
+  })
+  it('accepts size prop', () => {
+    const {getByTestId} = render(<DetailWithTheme size={1}/>)
+    expect(getByTestId('detail')).toHaveStyle(`
+      width: ${1 * 2}vw;
+    `)
+  })
+  it('has size without size prop', () => {
+    const {getByTestId} = render(<DetailWithTheme/>)
+    expect(getByTestId('detail')).toHaveStyle(`
+    width: 2vw;
+    `) 
+  })
+  it('has animation speed based on size', () => {
+    const {getByTestId} = render(<DetailWithTheme size={1}/>)
+    expect(getByTestId('detail')).toHaveStyle(`
+      animation-duration:2s;
+    `)
+  })
+  it('accepts verticalPosition prop - top', () => {
+    const {getByTestId} = render(<DetailWithTheme verticalPosition={'top'}/>)
+    expect(getByTestId('detail')).toHaveStyle(`
+      top:0%;
+    `)
+  })
+  it('accepts verticalPosition prop - unallowed value set', () => {
+    const {getByTestId} = render(<DetailWithTheme verticalPosition={'something'}/>)
+    expect(getByTestId('detail')).toHaveStyle(`
+      top:0%;
+    `)
+  })
+  it('accepts horizontalPosition prop - right', () => {
+    const {getByTestId} = render(<DetailWithTheme horizontalPosition={'right'}/>)
+    expect(getByTestId('detail')).toHaveStyle(`
+      right:0%;
+    `)
+  })
+  it('accepts horizontalPosition prop - unallowed value set', () => {
+    const {getByTestId} = render(<DetailWithTheme horizontalPosition={'something'}/>)
+    expect(getByTestId('detail')).toHaveStyle(`
+    left:0%;
+    `)
+  })
+  
+  describe('StyledRect', () => {
+    it('renders', () => {
+      const {getAllByTestId} = render(<DetailWithTheme/>)
+      expect(getAllByTestId('styled-rect')).not.toBe(null)
+    })
+    it('renders 4 rects', () => {
+      const {getAllByTestId} = render(<DetailWithTheme/>)
+      expect(getAllByTestId('styled-rect').length).toBe(4)
+    })
+    it('accepts theme fill', () => {
+      const {getAllByTestId} = render(<DetailWithTheme/>)
+      expect(getAllByTestId('styled-rect')[0]).toHaveStyle(`
+        fill:${theme.colors.accent};
+      `)
+    })
+  })
+})
