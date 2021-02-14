5352b000a1ba877d8eb69d2e970ffa00af9cb56e
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2021-02-14 19:42:44 +0100

    made contact form functional

--- squared-development/src/components/ContactForm.js
+++ squared-development/src/components/ContactForm.js
@@ -32,11 +32,65 @@ const ButtonContainer = styled.div`
   margin-top:10px;
 `
 
+const StatusMessage = styled.p`
+  width:100%;
+  padding:15px 10px;
+  text-align:center;
+  background:${props => props.fillColor ? (props.theme.colors[props.fillColor] ? props.theme.colors[props.fillColor] : props.theme.colors.primary) : props.theme.colors.primary};
+  color:${props => props.theme.colors.textLight};
+`
+
 const ContactForm = () => {
 
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState(undefined)
+  const [successMessage, setSuccessMessage] = useState(undefined)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setErrorMessage('')
+    setSuccessMessage('')
+    if (
+      !name ||
+      !message ||
+      !(
+        email &&
+        email.match(
+          /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+      )
+    ) {
+      setErrorMessage('Required fields not filled!')
+    } else {
+      setErrorMessage(undefined)
+      fetch(
+        `/.netlify/functions/contactForm?name=${name}&email=${email}&message=${message}`,
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          method: 'POST',
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.status == 'success') {
+            return setSuccessMessage(data.message)
+          } else if (data.status == 'error') {
+            return setErrorMessage(data.message)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          setErrorMessage(
+            'There was some error while trying to send your email. Try later!'
+          )
+        })
+    }
+  }
 
   return(
     <StyledContactForm>
@@ -48,8 +102,23 @@ const ContactForm = () => {
       <EmailInput placeholder='Email' value={email} onChange={setEmail}/>
       <TextareaInput placeholder='Message' value={message} onChange={setMessage}/>
       <ButtonContainer>
-        <Button primary>Send</Button>
+        <Button primary onClick={(e) => {
+          e.preventDefault()
+          handleSubmit(e)
+        }}>Send</Button>
       </ButtonContainer>
+      {errorMessage && (
+        <StatusMessage fillColor={'error'}>
+          {errorMessage}
+        </StatusMessage>
+      )}
+      {
+        successMessage && (
+        <StatusMessage fillColor={'success'}>
+          {successMessage}
+        </StatusMessage>
+        )
+      }
     </StyledContactForm>
   )
 }
