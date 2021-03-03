13d9dd995b32328f0ac1c21d456dd0ef0205e781
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2021-03-03 13:58:56 +0100

    made TransformedImageCard component with hover functionality

--- squared-development/src/components/Cards/TransformedImageCard.js
+++ squared-development/src/components/Cards/TransformedImageCard.js
@@ -11,8 +11,9 @@ import VerticalMarginSeparator from '../LayoutComponents/VerticalMarginSeparator
 const StyledImg = styled.img`
   max-width:100%;
   max-height:60vh;
-  transform-style:preserve-3d;
+  transition:0.6s;
   transform: rotate3d(0, 1, 0, -45deg);
+  transform-style:preserve-3d;
   box-shadow:0px 5px 25px rgba(0, 0, 0, 0.12);
   margin:50px 0px;
   ${props => props.reverse && css`
@@ -23,28 +24,62 @@ const StyledImg = styled.img`
   }  
 `
 
+const StyledCardTextContent = styled.div`
+  max-width:50%;
+  transition:max-width 0.6s;
+  overflow:hidden;
+  max-height:50vh;
+  h3{
+    margin-top:0;
+  }
+  @media (max-width:768px){
+    max-width:100%;
+    max-height:unset;
+  }
+`
+
 const StyledImageContainer = styled.div`
   max-width:50%;
   perspective:2000px;
   max-height:60vh;
+  transition:0.8s;
+  display:flex;
+  align-items:center;
+  justify-content: center;
+  &:hover{
+    @media (min-width:768px){
+      ~ div{
+        max-width:0%;
+      }
+      max-width:100%;
+      width:100%;
+      max-height:100vh;
+      ${StyledImg}{
+        transform: rotate3d(0, 1, 0, 0deg);
+        max-width:100%;
+        max-height:100vh;
+      }
+    }
+  }
   @media (max-width:768px){
-    width:100%;
+    max-width:100%;
   }
 `
 
 const StyledTransformedImageCard = styled.div`
   width:100%;
   display:flex;
-  flex-direction:row;
   justify-content:space-between;
+  flex-direction:row-reverse;
   align-items: center;
+  text-align:left;
   *{
     @media (max-width:768px){
       text-align:center;
     }  
   }
   ${props => props.reverse && css`
-    flex-direction:row-reverse;
+    flex-direction:row;
     *{
       text-align:right;
       @media (max-width:768px){
@@ -53,20 +88,17 @@ const StyledTransformedImageCard = styled.div`
     }
   `}
   @media (max-width:768px){
-    flex-direction:column-reverse;
+    flex-direction:column;
   }
 `
 
-const StyledCardTextContent = styled.div`
-  width:50%;
-  @media (max-width:768px){
-    width:100%;
-  }
-`
 
 const TransformedImageCard = ({image, title, description, buttonText, buttonLink, reverse}) => {
   return (
     <StyledTransformedImageCard reverse={reverse}>
+      <StyledImageContainer>
+        <StyledImg reverse={reverse} src={image} alt=""/>
+      </StyledImageContainer>
       <StyledCardTextContent>
         <Heading as={'h3'} display>
           {title}
@@ -79,9 +111,6 @@ const TransformedImageCard = ({image, title, description, buttonText, buttonLink
           {buttonText} 
         </Button>
       </StyledCardTextContent>
-      <StyledImageContainer>
-        <StyledImg reverse={reverse} src={image} alt=""/>
-      </StyledImageContainer>
     </StyledTransformedImageCard>
   )
 }
