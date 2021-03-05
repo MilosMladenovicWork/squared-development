03950fe2672538fa368407ab87916b7d26726cb5
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2021-03-05 13:36:48 +0100

    removed commented plugin from gatsby-config

--- squared-development/gatsby-config.js
+++ squared-development/gatsby-config.js
@@ -82,9 +82,8 @@ module.exports = {
         icon: `src/images/logo-square-min.svg`, // This path is relative to the root of the site.
       },
     },
-    "gatsby-plugin-offline"
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    "gatsby-plugin-offline"
   ],
 }
